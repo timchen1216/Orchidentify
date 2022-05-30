30888bf250ed6e3596f03819ac46781f055fed36
polaris0922 <75686553+polaris0922@users.noreply.github.com>
2022-05-30 22:42:23 +0800

    Update identify.py

--- service_actions/identify.py
+++ service_actions/identify.py
@@ -3,7 +3,6 @@ import pyimgur
 import pandas as pd
 import os
 from googlesearch import search
-from bs4 import beautifulsoup4
 
 df = pd.read_csv(os.path.join("service_actions","orchid_book.csv"), encoding = "Big5")
 
